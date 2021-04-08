a59dbf691309d1e1bc399139c1d93dce6e0aba3d
Roberta Bastos <rooitens@gmail.com>
2021-04-08 11:26:39 -0300

    Mais atualização

--- js/index.js
+++ js/index.js
@@ -22,19 +22,8 @@ alert(`- Calabresa: Mussarela, Calabresa e cebola\n
 - Lombo Canadense: Mussarela, Lombo Canadense, Tomate, Orégano, Cebola\n
 - X-Fritas: Mussarela, Contra-filé, Presunto, Bacon, Ovo, Tomate, Cebola, Batata Frita`);
 
-    var QuantidadeSabores=parseInt(prompt("Voce quer uma pizza com 1 ou 2 sabores:")); 
-    var saborUm=prompt(`Digite o número do sabor da pizza:\n
-    Relembrando as opções:\n
-        1- Calabresa\n
-        2- 4 queijos\n
-        3- Frango com Catupiry\n
-        4- Portuguesa\n
-        5- Camarão\n
-        6- Carne Seca\n
-        7- Lombo Canadense\n
-        8- X-Fritas`);   
-    if(QuantidadeSabores==2){
-    var sabordOIS=prompt(`Digite o número do segundo sabor:
+var QuantidadeSabores = parseInt(prompt("Voce quer uma pizza com 1 ou 2 sabores:"));
+var saborUm = prompt(`Digite o número do sabor da pizza:\n
     Relembrando as opções:\n
         1- Calabresa\n
         2- 4 queijos\n
@@ -44,4 +33,28 @@ alert(`- Calabresa: Mussarela, Calabresa e cebola\n
         6- Carne Seca\n
         7- Lombo Canadense\n
         8- X-Fritas`);
-    }
+if (QuantidadeSabores == 2) {
+    var sabordOIS = prompt(`Digite o número do segundo sabor:
+    Relembrando as opções:\n
+        1- Calabresa\n
+        2- 4 queijos\n
+        3- Frango com Catupiry\n
+        4- Portuguesa\n
+        5- Camarão\n
+        6- Carne Seca\n
+        7- Lombo Canadense\n
+        8- X-Fritas`);
+}
+
+var querbebida = prompt(`Você gostaria de uma bebida?`);
+
+if (querbebida[0] == "S" || querbebida[0] == "s") {
+    var tiposdebebidas = (prompt(`Digite o numero referente ao tipo de borda que voce quer:\n
+    1- Coca-cola (R$12,00)\n
+    2- Fanta (R$10,00)\n
+    3- Guaraná (R$10,00)\n
+    4- H2O (R$8,00)\n
+    5- Del Vale (R$7,00)`));
+}
+
+var tipoentrega = prompt(`Qual será a forma de entrega?\n 1- Balcão\n 2- Delivery`)
