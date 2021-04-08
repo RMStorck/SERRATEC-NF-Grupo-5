d9ba21597fdd58b4ec8cac43b0e94a20c51276b1
bluejynz <danifalk27@gmail.com>
2021-04-08 14:09:49 -0300

    um pouco de js

--- dany/js/utils.js
+++ dany/js/utils.js
@@ -1,1 +1,34 @@
-var precofinal = 0;
+var precoFinal = 0;
+var precisaTroco = false;
+
+var pedido = {
+    tamanho: "",
+    borda: "",
+    sabores: [],
+    bebida: ""
+};
+
+var pessoa = {
+    nome: "",
+    celular: ""
+};
+
+var endereco = {
+    rua: "",
+    numero: "",
+    complemento: "",
+    bairro: "",
+    pontoDeRef: ""
+};
+
+var formaDePagamento = [
+    "cartao de credito", "cartao de debito", "pix", "picpay", "dinheiro"
+];
+
+function makeNameCapitalize(value){
+    var nomes = value.toLowerCase().split(' ');
+            for(var i = 0; i < nomes.length; i++){
+                nomes[i] = nomes[i].charAt(0).toUpperCase() + nomes[i].substring(1);
+            }
+            return nomes.join(' ');
+}
