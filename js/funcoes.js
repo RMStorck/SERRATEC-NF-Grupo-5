d5e1f833073c5d6fec64caec0a1a55b8d3022b0d
Rosierabelo <rabelo.rosi@gmail.com>
2021-04-08 15:38:11 -0300

    Criando funções escolherBebida e escolherEntrega

--- js/funcoes.js
+++ js/funcoes.js
@@ -53,4 +53,55 @@ function escolherSabores(quantidaDeSabores) {
             break;
     }
 }
-
+function escolherBebida(querBebida) {
+    if (querBebida[0] == "S" || querBebida[0] == "s") {
+        var tiposDeBebidas = (prompt(`Digite o numero referente ao tipo de borda que voce quer:\n
+        1- Coca-cola (R$12,00)\n
+        2- Fanta (R$10,00)\n
+        3- Guaraná (R$10,00)\n
+        4- H2O (R$8,00)\n
+        5- Del Vale (R$7,00)`));
+    }
+}
+function escolherEntrega(entrega, nome) {
+    if (entrega == 2){
+        var delivery = [
+            celularDelivery = prompt(`${nome}, qual seu número de celular:`),
+            rua = prompt(`Insira o nome da sua rua:`),
+            bairro = prompt(`Insira o seu bairro:`),
+            numero = prompt(`Insira o número da sua residência?`),
+            complemento = prompt(`Insira o complemento?`),
+            pontoDeReferencia = prompt(`Insira um ponto de referência:`)
+        ]
+    }else{
+        celularBalcao = prompt(`${nome}, qual seu número de celular:`);
+    }
+}
+function escolherPagamento() {
+    var tipoDeEntrega = prompt(`Escolha a foma de pagamento: \n
+    1- Cartão Crédito
+    2- Cartão Débito
+    3- Pix
+    4- Picpay
+    5- Dinheiro`);
+    
+    if(tipoDeEntrega == 2 && formaDePagamento == 5){
+    
+        var precisaDeTroco = prompt(`Precisa de Troco?`);
+    
+        if (precisaDeTroco[0] == "S" || precisaDeTroco[0] == "s") {
+            var troco = (prompt(`Troco para quanto?`));
+        }
+    }
+}
+function cardapio(){
+   return alert(`- Calabresa: Mussarela, Calabresa e cebola\n
+        - 4 Queijos: Mussarela, Cheddar, Parmesão e Prato\n
+        - Frango com Catupiry :Mussarela, Frango e Catupiry\n
+        - Portuguesa: Mussarela, Pimentão, Cebola, Presunto, Ovo, Cebola\n
+        - Camarão: Mussarela, Camarão, Salsinha e Cebolinha\n
+        - Carne seca: Mussarela, Carne seca e cream cheese\n
+        - Lombo Canadense: Mussarela, Lombo Canadense, Tomate, Orégano, Cebola\n
+        - X-Fritas: Mussarela, Contra-filé, Presunto, Bacon, Ovo, Tomate, Cebola, Batata Frita`
+    );
+}
--- js/index.js
+++ js/index.js
@@ -1,56 +1,19 @@
 var nome = (prompt("Digite o seu nome:"));
-
 alert(`Seja bem vindo a Serra Pizza!\n${nome}, por favor, pressione enter para inciar o seu pedido:`);
+
 var tamanho = inserirTamanho();
 var tipoDeBorda = inserirBorda();
-alert(`Pronto, ${nome}.Agora, vou te mostrar a lista de sabores:`);
-alert(`- Calabresa: Mussarela, Calabresa e cebola\n
-- 4 Queijos: Mussarela, Cheddar, Parmesão e Prato\n
-- Frango com Catupiry :Mussarela, Frango e Catupiry\n
-- Portuguesa: Mussarela, Pimentão, Cebola, Presunto, Ovo, Cebola\n
-- Camarão: Mussarela, Camarão, Salsinha e Cebolinha\n
-- Carne seca: Mussarela, Carne seca e cream cheese\n
-- Lombo Canadense: Mussarela, Lombo Canadense, Tomate, Orégano, Cebola\n
-- X-Fritas: Mussarela, Contra-filé, Presunto, Bacon, Ovo, Tomate, Cebola, Batata Frita`);
 
-var quantidaDeSabores = escolherSabores(parseInt(prompt("Você quer uma pizza com 1 ou 2 sabores:")));
+alert(`Pronto, ${nome}. Agora, vou te mostrar a lista de sabores:`);
+cardapio();
+
+var quantidaDeSabores = (parseInt(prompt("Você quer uma pizza com 1 ou 2 sabores:")));
+escolherSabores(quantidaDeSabores);
 
 var querBebida = prompt(`Você gostaria de uma bebida?`);
-if (querBebida[0] == "S" || querBebida[0] == "s") {
-    var tiposDeBebidas = (prompt(`Digite o numero referente ao tipo de borda que voce quer:\n
-    1- Coca-cola (R$12,00)\n
-    2- Fanta (R$10,00)\n
-    3- Guaraná (R$10,00)\n
-    4- H2O (R$8,00)\n
-    5- Del Vale (R$7,00)`));
-}
+escolherBebida(querBebida);
 
 var tipoDeEntrega = prompt(`Qual será a forma de entrega?\n 1- Balcão\n 2- Delivery`);
-if (tipoDeEntrega == 2){
-    var delivery = [
-        celularDelivery = prompt(`${nome}, qual seu número de celular:`),
-        rua = prompt(`Digite o nome da sua rua:`),
-        bairro = prompt(`Digite o seu bairro:`),
-        numero = prompt(`Qual o número da sua residência:`),
-        complemento = prompt(`Digite o complemento:\n Caso aja, caso não, deixe dê enter`),
-        pontoDeReferencia = prompt(`Digite um ponto de referência:`)
-    ]
-}else{
-    celularBalcao = prompt(`${nome}, qual seu número de celular:`);
-}
+escolherEntrega(tipoDeEntrega, nome);
 
-var formaDePagamento = prompt(`Escolha a foma de pagamento: \n
-1- Cartão Crédito
-2- Cartão Débito
-3- Pix
-4- Picpay
-5- Dinheiro`);
-
-if(tipoDeEntrega == 2 && formaDePagamento == 5){
-
-    var precisaDeTroco = prompt(`Precisa de Troco?`);
-
-    if (precisaDeTroco[0] == "S" || precisaDeTroco[0] == "s") {
-        var troco = (prompt(`Quanto precisará de troco:`));
-    }
-}
+var formaDePagamento = escolherEntrega()
