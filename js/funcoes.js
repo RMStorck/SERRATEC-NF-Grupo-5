348ddc0df37e35459bf93b833cced15a76ced83a
Rosierabelo <rabelo.rosi@gmail.com>
2021-04-08 14:33:46 -0300

    Consertanto função escolherSabores

--- js/funcoes.js
+++ js/funcoes.js
@@ -16,41 +16,45 @@ function inserirBorda() {
     return tipoDeBorda;
 }
 function escolherSabores(quantidaDeSabores) {
-    switch (quantidaDeSabores){
-        case 1:
-            var sabor = prompt(`Digite o número do sabor da pizza:\n
-            Relembrando as opções:\n
-                1- Calabresa\n
-                2- 4 queijos\n
-                3- Frango com Catupiry\n
-                4- Portuguesa\n
-                5- Camarão\n
-                6- Carne Seca\n
-                7- Lombo Canadense\n
-                8- X-Fritas`);
-            break;
-        case 2:
-            var saborUm = prompt(`Digite o número do sabor da pizza:\n
-            Relembrando as opções:\n
-                1- Calabresa\n
-                2- 4 queijos\n
-                3- Frango com Catupiry\n
-                4- Portuguesa\n
-                5- Camarão\n
-                6- Carne Seca\n
-                7- Lombo Canadense\n
-                8- X-Fritas`);
-            var saborDois = prompt(`Digite o número do segundo sabor:
-            Relembrando as opções:\n
-                1- Calabresa\n
-                2- 4 queijos\n
-                3- Frango com Catupiry\n
-                4- Portuguesa\n
-                5- Camarão\n
-                6- Carne Seca\n
-                7- Lombo Canadense\n
-                8- X-Fritas`);
-            break;
-        default: prompt(`Por favor, insira novamente a quantidade de sabores: 1 ou 2.`);
+    if (quantidaDeSabores != 1 || quantidaDeSabores != 2) {
+        prompt(`Por favor, insira novamente a quantidade de sabores: 1 ou 2.`);
+        escolherSabores(quantidaDeSabores);
+    } else if(quantidaDeSabores == 1 || quantidaDeSabores == 2){
+        switch(quantidaDeSabores) {
+            case 1:
+                var sabor = prompt(`Digite o número do sabor da pizza:\n
+                    Relembrando as opções:\n
+                    1- Calabresa\n
+                    2- 4 queijos\n
+                    3- Frango com Catupiry\n
+                    4- Portuguesa\n
+                    5- Camarão\n
+                    6- Carne Seca\n
+                    7- Lombo Canadense\n
+                    8- X-Fritas`);
+                break;
+            case 2:
+                var saborUm = prompt(`Digite o número do sabor da pizza:\n
+                    Relembrando as opções:\n
+                    1- Calabresa\n
+                    2- 4 queijos\n
+                    3- Frango com Catupiry\n
+                    4- Portuguesa\n
+                    5- Camarão\n
+                    6- Carne Seca\n
+                    7- Lombo Canadense\n
+                    8- X-Fritas`);
+                var saborDois = prompt(`Digite o número do segundo sabor:
+                    Relembrando as opções:\n
+                    1- Calabresa\n
+                    2- 4 queijos\n
+                    3- Frango com Catupiry\n
+                    4- Portuguesa\n
+                    5- Camarão\n
+                    6- Carne Seca\n
+                    7- Lombo Canadense\n
+                    8- X-Fritas`);
+                break;
+        }
     }
 }
