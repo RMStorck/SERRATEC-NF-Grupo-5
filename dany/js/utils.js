cc670bd05d50d1f03db49606549b33a5f1e92c6e
bluejynz <danifalk27@gmail.com>
2021-04-08 15:14:56 -0300

    função quantidade de sabores

--- dany/js/utils.js
+++ dany/js/utils.js
@@ -1,36 +1,84 @@
-var precoFinal = 0;
-var precisaTroco = false;
+// var precoFinal = 0;
+// var precisaTroco = false;
 
-var pedido = {
-    tamanho: "",
-    borda: "",
-    sabores: [],
-    bebida: ""
-};
+// var pedido = {
+//     tamanho: "",
+//     borda: "",
+//     sabores: [],
+//     bebida: ""
+// };
 
-var pessoa = {
-    nome: "",
-    celular: ""
-};
+// var pessoa = {
+//     nome: "",
+//     celular: ""
+// };
 
-var endereco = {
-    rua: "",
-    numero: "",
-    complemento: "",
-    bairro: "",
-    pontoDeRef: ""
-};
+// var endereco = {
+//     rua: "",
+//     numero: "",
+//     complemento: "",
+//     bairro: "",
+//     pontoDeRef: ""
+// };
 
-var formaDePagamento = [
-    "cartao de credito", "cartao de debito", "pix", "picpay", "dinheiro"
-];
+// var formaDePagamento = [
+//     "cartao de credito", "cartao de debito", "pix", "picpay", "dinheiro"
+// ];
 
-function makeNameCapitalize(value){
-    var nomes = value.toLowerCase().split(' ');
-            for(var i = 0; i < nomes.length; i++){
-                nomes[i] = nomes[i].charAt(0).toUpperCase() + nomes[i].substring(1);
-            }
-            return nomes.join(' ');
+// function makeNameCapitalize(value){
+//     var nomes = value.toLowerCase().split(' ');
+//             for(var i = 0; i < nomes.length; i++){
+//                 nomes[i] = nomes[i].charAt(0).toUpperCase() + nomes[i].substring(1);
+//             }
+//             return nomes.join(' ');
+// }
+
+// // if(document.getElementsByTagName("tamanhoPizza"))
+
+
+
+var qtdDeSabores = parseInt(prompt("Quer quantos sabores? 2 ou 1"));
+choose(qtdDeSabores);
+function choose(qtd){
+    if(qtd > 2 || qtd < 1){
+        qtdDeSabores = parseInt(prompt("Digite a quantidade certa de sabores. 2 ou 1?"));
+        choose(qtdDeSabores);
+    } else{
+        switch(quantidaDeSabores) {
+            case 1:
+                var sabor = prompt(`Digite o número do sabor da pizza:\n
+                    Relembrando as opções:\n
+                    1- Calabresa\n
+                    2- 4 queijos\n
+                    3- Frango com Catupiry\n
+                    4- Portuguesa\n
+                    5- Camarão\n
+                    6- Carne Seca\n
+                    7- Lombo Canadense\n
+                    8- X-Fritas`);
+                break;
+            case 2:
+                var saborUm = prompt(`Digite o número do sabor da pizza:\n
+                    Relembrando as opções:\n
+                    1- Calabresa\n
+                    2- 4 queijos\n
+                    3- Frango com Catupiry\n
+                    4- Portuguesa\n
+                    5- Camarão\n
+                    6- Carne Seca\n
+                    7- Lombo Canadense\n
+                    8- X-Fritas`);
+                var saborDois = prompt(`Digite o número do segundo sabor:
+                    Relembrando as opções:\n
+                    1- Calabresa\n
+                    2- 4 queijos\n
+                    3- Frango com Catupiry\n
+                    4- Portuguesa\n
+                    5- Camarão\n
+                    6- Carne Seca\n
+                    7- Lombo Canadense\n
+                    8- X-Fritas`);
+                break;
+        }
+    }
 }
-
-// if(document.getElementsByTagName("tamanhoPizza"))
